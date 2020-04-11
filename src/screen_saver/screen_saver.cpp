48a2b67a7be6b681fa875f10cc5983faca386205
Rafael Correa <rafael.correa@sap.com>
2020-04-11 20:26:38 -0300

    Extracted reflection to methods

--- src/screen_saver/screen_saver.cpp
+++ src/screen_saver/screen_saver.cpp
@@ -8,14 +8,34 @@
 #include <common/shaders.h>
 #include <common/vaoConfig.h>
 #include <common/vboConfig.h>
+#include <common/reflection.h>
 
 const glm::vec2 SCREEN_SIZE(800, 600);
 
+GLfloat getNormal(GLfloat lastPosition){
+    return lastPosition > 0 ? 1 : -1;
+}
+GLfloat calculateReflection(GLfloat normal, GLfloat speed){
+    return 2 * normal * (normal * - speed) + speed;
+}
+
+GLfloat calculateNextPosition(GLfloat elapsedSeconds, GLfloat speed, GLfloat lastPosition){
+    return elapsedSeconds * speed + lastPosition;
+}
+
+GLfloat hasHitAWall(GLfloat lastPosition){
+    return fabs(lastPosition) + 0.5f > 1.0f;
+}
+
+GLfloat getReflectionPosition(GLfloat lastPosition, GLfloat speed){
+    if (hasHitAWall(lastPosition)) {
+        GLfloat normal = getNormal(lastPosition);
+        return calculateReflection(normal, speed);
+    }
+    return speed;
+}
+
 int main () {
-	const GLubyte* renderer;
-	const GLubyte* version;
-	GLuint vbo, colorsVBO;
-	
     GLfloat points[]={
          0.0f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
@@ -49,25 +69,25 @@ int main () {
     VboConfig colorsVbo(colors);
     vao.bind(1);
 
-	glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
-
-	float speed = 1.0f;
-	float lastPosition = 0.0f;
+    glm::vec2 speed (.5f, .6f);
+    glm::vec2 lastPosition (.0f, .0f);
 	ourShader.use();
 	while (!glfwWindowShouldClose (glfw.getWindow())) {
 	
-		static double previousSeconds = glfwGetTime();
-		double currentSeconds = glfwGetTime();
-		double elapsedSeconds = currentSeconds - previousSeconds;
-		if(elapsedSeconds > 0) {
-			previousSeconds = currentSeconds;
-			if(fabs(lastPosition) > 1.0f){
-				speed = -speed;
-			}
-			matrix[3][0] = elapsedSeconds * speed + lastPosition;
-			lastPosition = matrix[3][0];
-		}
-		
+        static double previousSeconds = glfwGetTime();
+        double currentSeconds = glfwGetTime();
+        double elapsedSeconds = currentSeconds - previousSeconds;
+        if(elapsedSeconds > 0) {
+            previousSeconds = currentSeconds;
+            speed.x = getReflectionPosition(lastPosition.x, speed.x);
+            speed.y = getReflectionPosition(lastPosition.y, speed.y);
+
+            matrix[3][0] = calculateNextPosition(elapsedSeconds, speed.x, lastPosition.x);
+            matrix[3][1] = calculateNextPosition(elapsedSeconds, speed.y, lastPosition.y);
+            lastPosition.x = matrix[3][0];
+            lastPosition.y = matrix[3][1];
+        }
+
         ourShader.setMat4("matrix",matrix);
 	
 		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);

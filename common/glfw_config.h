8dc68a5b67848a77e2f9fa5eea50b3563743325b
Fabiane <fabianeekuhn@gmail.com>
2020-04-11 22:58:52 -0300

    Change window background from blue to gray

--- common/glfw_config.h
+++ common/glfw_config.h
@@ -5,7 +5,7 @@
 #include <glm/glm.hpp>
 #include <common/controls.hpp>
 
-#define DEFAULT_WINDOW_TITLE "LearnOpenGL"
+#define DEFAULT_WINDOW_TITLE "Screen Saver"
 
 class GlfwConfig {
     public:
@@ -35,8 +35,8 @@ class GlfwConfig {
             glfwMakeContextCurrent(window);
             // Ensure we can capture the escape key being pressed below
             glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-            // Dark blue background
-            glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+            // Gray background
+            glClearColor(0.8f, 0.8f, 0.8f, 0.0f);
             return 1;
         }
 
